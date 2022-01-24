9ffcb10cbc66b2cfa753d41ec23fa8c873511651
MeganSorenson <85324120+MeganSorenson@users.noreply.github.com>
2022-01-24 11:33:03 -0700

    checked board number entries and col/row entries

--- lab/lab3/lab3_NumTicTacToe.py
+++ lab/lab3/lab3_NumTicTacToe.py
@@ -1,13 +1,8 @@
 # ----------------------------------------------------
 # Lab 3: Numerical Tic Tac Toe class
 #
-# Author:
-# Collaborators:
-# References:
+# Author: Megan Sorenson
 # ----------------------------------------------------
-
-# SHOULD I BE CHECKING IF THE NUMBER FROM 1-9 IS ALREADY ON THE BOARD?
-
 
 class NumTicTacToe:
     def __init__(self):
@@ -18,6 +13,9 @@ class NumTicTacToe:
         '''
         self.board = []  # list of lists, where each internal list represents a row
         self.size = 3   # number of columns and rows of board
+        # which numbers are still available to input
+        self.available_board_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.row_col_options = (0, 1, 2)
 
         # populate the empty squares in board with 0
         for i in range(self.size):
@@ -28,7 +26,7 @@ class NumTicTacToe:
 
     def drawBoard(self):
         '''
-        Displays the current state of the board, formatted with column and row 
+        Displays the current state of the board, formatted with column and row
         indicies shown.
         Inputs: none
         Returns: None
@@ -70,7 +68,7 @@ class NumTicTacToe:
 
     def squareIsEmpty(self, row, col):
         '''
-        Checks if a given square is empty, or if it already contains a number 
+        Checks if a given square is empty, or if it already contains a number
         greater than 0.
         Inputs:
            row (int) - row index of square to check
@@ -78,14 +76,17 @@ class NumTicTacToe:
         Returns: True if square is empty; False otherwise
         '''
         # TO DO: delete pass and complete method
-        if self.board[row][col] > 0:
-            return False
+        if row in self.row_col_options and col in self.row_col_options:  # check if row and col valid
+            if self.board[row][col] > 0:
+                return False
+            else:
+                return True
         else:
-            return True
+            return False
 
     def update(self, row, col, num):
         '''
-        Assigns the integer, num, to the board at the provided row and column, 
+        Assigns the integer, num, to the board at the provided row and column,
         but only if that square is empty.
         Inputs:
            row (int) - row index of square to update
@@ -94,8 +95,11 @@ class NumTicTacToe:
         Returns: True if attempted update was successful; False otherwise
         '''
         # TO DO: delete pass and complete method
-        if self.squareIsEmpty(row, col):
+        # check if the square is empty and the number hasn't already on the board
+        if self.squareIsEmpty(row, col) and num in self.available_board_numbers:
             self.board[row][col] = num
+            # make that number unavailable for future use
+            self.available_board_numbers.remove(num)
             return True
         else:
             return False
@@ -168,10 +172,10 @@ if __name__ == "__main__":
     myBoard.drawBoard()
 
     # try to assign a number to a non-empty square. What happens?
-    #num = int(input('Player, please enter a number (1-9): '))
-    #row = int(input('Player, please enter a row: '))
-    #col = int(input('Player 1, please enter a column: '))
-    #myBoard.update(row, col, num)
+    # num = int(input('Player, please enter a number (1-9): '))
+    # row = int(input('Player, please enter a row: '))
+    # col = int(input('Player 1, please enter a column: '))
+    # myBoard.update(row, col, num)
     # myBoard.drawBoard()
 
     # check if the board has a winner. Should there be a winner after only 1 entry?
