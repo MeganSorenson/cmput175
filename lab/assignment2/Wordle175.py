578529e91a28feb5d7b71f9a0fb40517032be984
MeganSorenson <85324120+MeganSorenson@users.noreply.github.com>
2022-02-01 00:02:55 -0700

    as2 ScrabbleDict tested

--- lab/assignment2/Wordle175.py
+++ lab/assignment2/Wordle175.py
@@ -23,7 +23,7 @@ class ScrabbleDict:
         for line in lines:
             word = line.split(' ')[0]
             if len(word) == size:
-                self.word_dict[word] = line
+                self.word_dict[word] = line.strip()
 
     def check(self, word):
         '''
@@ -57,8 +57,9 @@ class ScrabbleDict:
         for word in self.word_dict.keys():
             if word[0] == letter:
                 word_list.append(word)
+        word_list.sort()
 
-        return word_list.sort()
+        return word_list
 
     def getWordSize(self):
         '''
@@ -71,4 +72,62 @@ class ScrabbleDict:
 
 if __name__ == '__main__':
     # tests the ScrabbleDict class and its methods
-    pass
+
+    # create test file
+    test_file = open('test.txt', 'w')
+    lines = ['apple is a fruit', 'amber is a color',
+             'attic is a room', 'basil is a herb']
+    for line in lines:
+        test_file.write('{line}\n'.format(line=line))
+    test_file.close()
+
+    # create dictionary
+    word_size = 5
+    s = ScrabbleDict(word_size, 'test.txt')
+    # view initialized dictionary
+    print('initialized dictionary: ', end='')
+    print(s.word_dict)
+
+    # TEST 1: check if word size is the same as initialized
+    print('\nTEST 1: check getWordSize()')
+    if s.getWordSize() == word_size:
+        print('test1: word size same as initialized')
+        print('test1: PASSED')
+    else:
+        print('test1: word size not the same as initialized')
+        print("test1: FAILED")
+
+    # TEST 2: check getWords()
+    print('\nTEST 2: check getWords() for the letter a')
+    letter = 'a'
+    number_a_words = 3  # determined from when test.txt was created
+    word_list = s.getWords(letter)
+    print('test2: words starting with a: {word_list}'.format(
+        word_list=word_list))
+    if len(word_list) == number_a_words:
+        print('test2: expected number of {letter} words in dictionary'.format(
+            letter=letter))
+        print('test2: PASSED')
+    else:
+        print('test2: unexpected number of {letter} words in dictionary'.format(
+            letter=letter))
+        print('test2: FAILED')
+
+    # TEST 3: check getSize()
+    print('\nTEST 3: check getSize()')
+    if s.getSize() == len(lines):
+        print('test3: expected number of words in dictionary')
+        print('test3: PASSED')
+    else:
+        print('test3: unexpected number of words in dictionary')
+        print('test3: FAILED')
+
+    # TEST 4: check check()
+    print('\nTEST 4: check check() using known word in dictionary')
+    known_word = 'basil'
+    if s.check(known_word):
+        print('test4: known word in dictionary')
+        print('test4: PASSED')
+    else:
+        print('test4: known word not in dictionary')
+        print('test4: FAILED')
